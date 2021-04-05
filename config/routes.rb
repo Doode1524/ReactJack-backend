29124daf3a192fa7c6eb2de1a999d170339fd1b5
Doode1524 <Doode1524@gmail.com>
2021-04-05 15:52:24 -0400

    sessions init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create, :show, :index]
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
